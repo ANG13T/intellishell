d74010b624cdaf1a4fa77cc39262be5599c1d114
angelina-tsuboi <angelina.t1832@gmail.com>
2024-06-28 20:53:23 -0700

    feat: update analyzer

--- README.md
+++ README.md
@@ -3,11 +3,13 @@
 - generate a Gemfile
 
 ### Features
-- [] Autosave into a directory with name as a timestamp instance 
-- [] Summarize the session using AI and then save into the header section of the file
+- [x] Autosave into a directory with name as a timestamp instance 
+- [x] Summarize the session using AI and then save into the header section of the file
 - [] Make a global table log of all the sessions: name, timestamp, and one line summary of the session
 - [] Configure flag for visual display of table logs 
 - [] Easily find a session via search and AI calibration
 
 ### EDIT a COOL SCREENSHARE with a cool transition of the logo
 - [] Add a cool logo
+- [] screenshare 
+- [] github
--- analyzer.rb
+++ analyzer.rb
@@ -1,6 +1,7 @@
 require 'net/http'
 require 'json'
 require 'yaml'
+require 'csv'
 
 OPENAI_API_ENDPOINT = URI('https://api.openai.com/v1/chat/completions')
 
@@ -105,8 +106,8 @@ def analyze_terminal_code(path, summary_path, timestamp)
     response = ask_chatgpt(terminal_contents)
     puts "ChatGPT says: #{response}"
     parsed_output = parse_response(response)
-    save_summary(summary_path, parsed_output)
-    save_to_catalog(response, timestamp)
+    save_summary(summary_path, parsed_output, timestamp)
+    save_to_catalog(response, timestamp, path)
   rescue => e
     puts "Error: #{e.message}"
   end
@@ -128,17 +129,23 @@ def parse_response(response)
   }
 end
 
-def save_summary(summary_path, contents)
+def save_summary(summary_path, contents, timestamp)
 
   # Save the summary to a file
-  File.open(summary_path, 'w') { |file| file.write("TITLE: #{contents[:title]} \n SUMMARY: #{contents[:summary]}") }
+  File.open(summary_path, 'w') { |file| file.write("TITLE: #{contents[:title]} \nTIMESTAMP:#{timestamp}\nSUMMARY: #{contents[:summary]}") }
 end
 
 # Saves the session to a CSV file
 # Timestamp, Path, Summary, Description
-def save_to_catalog(input, timestamp)
+def save_to_catalog(input, timestamp, path)
+  puts input.class, timestamp.class, path.class,  input[:summary].class, input[:description].class
+  # write the the sessions.csv file and log the terminal entry
+  session_entry = [path, input[:summary].to_s, input[:description]]
+  session_catalog = load_config['session_catalog']
 
-
+  CSV.open(session_catalog, 'a+') do |csv|
+    csv << session_entry
+  end
 end
 
 env_check
